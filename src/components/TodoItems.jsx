48b379fb4b03b3ab04eda6eb741f78ea5c2cae82
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-09 08:57:56 +0300

    feat: added dont have todo alert

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -4,13 +4,19 @@ import TodoItem from './TodoItem'
 
 const TodoItems = ({ todos, deleteTodo }) => (
   <ul className='todo-items list-group my-2'>
-    {todos.map((todo) => (
-      <TodoItem
-        key={todo.id}
-        todo={todo}
-        deleteTodo={() => deleteTodo(todo.id)}
-      />
-    ))}
+    {todos.length > 0 ? (
+      todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          deleteTodo={() => deleteTodo(todo.id)}
+        />
+      ))
+    ) : (
+      <div className='dont-have-todos-alert alert alert-light'>
+        You don&apos;t have any todo
+      </div>
+    )}
   </ul>
 )
 
