593df952ef093ad77e17799b20b7aec398d5d899
Yuri Mikushov <mikushov.yu@gmail.com>
2021-03-10 09:01:20 +0300

    feat: add todo don't editable mode

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { observer } from 'mobx-react-lite'
 
-const TodoItem = ({ todo, deleteTodo }) => (
-  <li className='todo-item d-flex align-items-center shadow-sm rounded p-3 my-1'>
-    <input
-      className='todo-item__done'
-      type='checkbox'
-      checked={todo.done}
-      onChange={() => todo.toggle()}
-    />
-    <input
-      className='todo-item__name form-control mx-3'
-      type='text'
-      value={todo.title}
-      placeholder='Type a todo'
-      onChange={(e) => todo.setTitle(e.target.value)}
-    />
-    <button
-      className='todo-item__delete-btn btn close fs-1'
-      onClick={deleteTodo}
+const TodoItem = ({ todo, deleteTodo }) => {
+  const [todoEditable, setTodoEditable] = useState(false)
+
+  return (
+    <li
+      className='todo-item d-flex align-items-center shadow-sm rounded p-3 my-1'
+      onClick={() => setTodoEditable(true)}
     >
-      <span>&times;</span>
-    </button>
-  </li>
-)
+      <input
+        className='todo-item__done'
+        type='checkbox'
+        checked={todo.done}
+        onChange={() => todo.toggle()}
+      />
+      {todoEditable ? (
+        <input
+          className='todo-item__name form-control mx-3'
+          type='text'
+          value={todo.title}
+          placeholder='Type a todo'
+          onChange={(e) => todo.setTitle(e.target.value)}
+          onBlur={() => setTodoEditable(false)}
+          autoFocus={true}
+        />
+      ) : (
+        <div className='todo-item__name w-100 mx-3'>{todo.title}</div>
+      )}
+      <button
+        className='todo-item__delete-btn btn close ml-auto'
+        onClick={deleteTodo}
+      >
+        <span>&times;</span>
+      </button>
+    </li>
+  )
+}
 
 TodoItem.propTypes = {
   todo: PropTypes.shape({
